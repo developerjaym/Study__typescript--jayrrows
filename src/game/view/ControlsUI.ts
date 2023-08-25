6f7527a97e5f25c55bbd0f72664d305187a8ee7e
Jay M <jaymansmann@yahoo.com>
2023-08-25 16:20:47 -0400

    Better rules, show rules on start, adds og stuff

--- src/game/view/ControlsUI.ts
+++ src/game/view/ControlsUI.ts
@@ -1,29 +1,54 @@
 import { HTMLService } from "../../service/HTMLService.js";
 import injector from "../../service/Injector.js";
+import { rules } from "../../text/Rules.js";
 import { GameController } from "../controller/GameController.js";
-import { GameEvent } from "../model/GameEvent.js";
+import { GameEvent, GameEventType } from "../model/GameEvent.js";
 import { Icon } from "./Icon.js";
 import { Viewable } from "./Viewable.js";
 
 export class ControlsUI implements Viewable {
-    constructor(private controller: GameController, private htmlService: HTMLService = injector.getHtmlService()) {
-
+    private helpDialogShown = false
+  constructor(
+    private controller: GameController,
+    private htmlService: HTMLService = injector.getHtmlService()
+  ) {}
+  get component(): HTMLElement {
+    const container = this.htmlService.create(
+      "section",
+      ["controls"],
+      "controls"
+    );
+    const undoButton = this.htmlService.create(
+      "button",
+      ["button", "button--game"],
+      "undoButton",
+      "Undo"
+    );
+    undoButton.addEventListener("click", () => this.controller.undo());
+    const endGameButton = this.htmlService.create(
+      "button",
+      ["button", "button--game"],
+      "endGameButton",
+      "End Game"
+    );
+    endGameButton.addEventListener("click", () => this.controller.endGame());
+    const helpButton = this.htmlService.create(
+      "button",
+      ["button", "button--game"],
+      "helpButton",
+      "Help"
+    );
+    helpButton.addEventListener("click", () => this.showHelpDialog());
+    container.append(undoButton, endGameButton, helpButton);
+    return container;
+  }
+  onChange(event: GameEvent): void {
+    if (event.type === GameEventType.START && !this.helpDialogShown) {
+      this.showHelpDialog();
+      this.helpDialogShown = true
     }
-    get component(): HTMLElement {
-        const container = this.htmlService.create("section", ["controls"], "controls")
-        const undoButton = this.htmlService.create("button", ["button", "button--game"], "undoButton", "Undo")
-        undoButton.addEventListener("click", () => this.controller.undo())
-        const endGameButton = this.htmlService.create("button", ["button", "button--game"], "endGameButton", "End Game")
-        endGameButton.addEventListener("click", () => this.controller.endGame())
-        const helpButton = this.htmlService.create("button", ["button", "button--game"], "helpButton", "Help")
-        helpButton.addEventListener("click", () => this.showHelpDialog())
-        container.append(undoButton, endGameButton, helpButton);
-        return container;
-    }
-    onChange(event: GameEvent): void {
-    }
-    private showHelpDialog() {
-        this.htmlService.showDialog("Help", `PIECES\n King (${Icon.KING_PIECE}): This piece cannot move.\n  Your goal is to capture your opponent's King.\n  The first piece you move becomes your King.\n Diagonal-Mover (${Icon.DIAGONAL_PIECE}): This piece can move one space diagonally.\n Vertical-Mover (${Icon.VERTICAL_PIECE}): This piece can move one space vertically.\n Horizontal-Mover (${Icon.HORIZONTAL_PIECE}): This piece can move one space horizontally.\n All-Mover (${Icon.ALL_PIECE}): This piece can move to any connecting square.\n  The first All-Mover you move is promoted to King.\n  Later, an All-Mover is promoted based on direction you moved it.\n   So, if moved diagonally, it becomes a Diagonal-Mover.\n\nPLAY\n Green goes first.\n Purple goes second.\n Moving a piece to an occupied square will capture the opponent's piece.\n Capturing a King ends the game.`, Icon.HELP)
-
-    }
+  }
+  private showHelpDialog() {
+    this.htmlService.showDialog("Help", rules, Icon.HELP);
+  }
 }
